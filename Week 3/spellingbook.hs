74d4731efa6ae38a88280d89ee052c22d77bb5fc
Ashley McGeechan <apmcgeechan@gmail.com>
2018-09-23 22:16:14 +0100

    Week 3 partial completion

---
+++ Week 3/spellingbook.hs
@@ -0,0 +1,6 @@
+wordlist = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", 
+    "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"]
+
+speller :: [[Char]] -> [Char]
+speller wordlist =
+    
